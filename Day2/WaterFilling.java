fd14ac3a934971891b549760da0ebb5e498a3336
ArpitaGupta541 <arpita1475@gmail.com>
2026-03-12 23:18:28 +0530

    Day 2 Water Filling problem

---
+++ Day2/WaterFilling.java
@@ -0,0 +1,19 @@
+
+import java.util.*;
+
+public class WaterFilling {
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            if (x * y >= 100) {
+                System.out.println((x * y) / 100);
+            } else {
+                System.out.println("0");
+            }
+        }
+    }
+}
