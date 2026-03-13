9490417dc24e624ff3ecebcd933036ed1c17a37e
ArpitaGupta541 <arpita1475@gmail.com>
2026-03-13 22:37:01 +0530

    Day 3: Added Sale Season Codechef problem solution

---
+++ Day3/SaleSeason541.java
@@ -0,0 +1,22 @@
+
+import java.util.*;
+
+class SaleSeason541 {
+
+    public static void main(String[] args) throws java.lang.Exception {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int x = sc.nextInt();
+            if (x <= 100) {
+                System.out.println(x); 
+            }else if (x > 100 && x <= 1000) {
+                System.out.println(x - 25); 
+            }else if (x > 1000 && x <= 5000) {
+                System.out.println(x - 100); 
+            }else {
+                System.out.println(x - 500);
+            }
+        }
+    }
+}
