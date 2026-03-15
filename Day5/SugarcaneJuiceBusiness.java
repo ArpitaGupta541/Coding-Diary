9528ccf6f81818fb1441575abcdc1b59461822b2
ArpitaGupta541 <arpita1475@gmail.com>
2026-03-15 23:46:50 +0530

    Problem 563 on Sugarcane Juice Business on Codechef

---
+++ Day5/SugarcaneJuiceBusiness.java
@@ -0,0 +1,15 @@
+//Sugarcane Juice Business
+
+import java.util.*;
+
+class SugarcaneJuiceBusiness {
+
+    public static void main(String[] args) throws java.lang.Exception {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            int N = sc.nextInt();
+            System.out.println(15 * N);
+        }
+    }
+}
