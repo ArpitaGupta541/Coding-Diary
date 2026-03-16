45386af5828ae72d912a1ca0468357c570904514
ArpitaGupta541 <arpita1475@gmail.com>
2026-03-16 22:31:16 +0530

    Problem 563 Count the Notebooks on Codechef

---
+++ Day6/CounttheNotebooks.java
@@ -0,0 +1,15 @@
+//CounttheNotebooks
+
+import java.util.*;
+
+class CounttheNotebooks {
+
+    public static void main(String[] args) throws java.lang.Exception {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int x = sc.nextInt();
+            System.out.println(x * 10);
+        }
+    }
+}
