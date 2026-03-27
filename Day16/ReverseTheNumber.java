68f0bd2365da657d9878da30f52c2f29bb7b0773
ArpitaGupta541 <arpita1475@gmail.com>
2026-03-27 22:33:19 +0530

    Problem 588 Reverse The Number on Codechef

---
+++ Day16/ReverseTheNumber.java
@@ -0,0 +1,21 @@
+//ReverseTheNumber
+
+import java.util.*;
+
+class ReverseTheNumber {
+
+    public static void main(String[] args) throws java.lang.Exception {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int x = sc.nextInt();
+            int rev = 0;
+            while (x > 0) {
+                int digit = x % 10;
+                rev = rev * 10 + digit;
+                x = x / 10;
+            }
+            System.out.println(rev);
+        }
+    }
+}
